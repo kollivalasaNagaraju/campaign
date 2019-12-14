956d83415794fa84bd00717355a78b0af52bba93
kollivalasaNagaraju <nagarajubasara@gmail.com>
2019-12-14 11:08:46 +0530

    Add files via upload

---
+++ campaignManagement/src/main/java/com/campaignManagement/campaignManagement/model/MailRequest.java
@@ -0,0 +1,35 @@
+package com.campaignManagement.campaignManagement.model;
+
+import java.util.List;
+
+public class MailRequest {
+	String html;
+	String emailId;
+	String subject;
+	List<String> filter;
+	public String getHtml() {
+		return html;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public void setHtml(String html) {
+		this.html = html;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public List<String> getFilter() {
+		return filter;
+	}
+	public void setFilter(List<String> filter) {
+		this.filter = filter;
+	}
+	
+}
